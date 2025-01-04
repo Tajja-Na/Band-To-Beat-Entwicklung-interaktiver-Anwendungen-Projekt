4d929c9f242ec57151b84f4c841d34258ba033ac
calypsivano <laura.m.l98@hotmail.de>
2025-01-05 00:50:49 +0100

    Welcome Screen 01 angelegt

--- src/main/java/eiboprojekt/presentation/scenes/GameView/Welcome.java
+++ src/main/java/eiboprojekt/presentation/scenes/GameView/Welcome.java
@@ -1,10 +1,14 @@
 package eiboprojekt.presentation.scenes.GameView;
 
+import java.io.File;
+
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 
 public class Welcome extends BorderPane {
 
@@ -15,22 +19,42 @@ public class Welcome extends BorderPane {
         setMinSize(width, height);
         setMaxSize(width, height);
 
+        getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
         initializeUI();
     }
 
     private void initializeUI() {
+        // Center Container
         VBox centerBox = new VBox(20); // 20 ist der vertikale Abstand zwischen Elementen
-        centerBox.setAlignment(Pos.CENTER);
+        centerBox.setAlignment(Pos.TOP_CENTER);
 
-        Text welcomeLabel = new Text("Willkommen");
+        // Logo
+        ImageView logoView = new ImageView(new Image(new File("assets/Welcome/Logo.png").toURI().toString()));
+        logoView.setPreserveRatio(true);
+        logoView.setFitWidth(750); // Breite des Logos
+        // Button
         switchButton = new Button("Press Start!");
+        switchButton.getStyleClass().add("start-button");
 
-        centerBox.getChildren().addAll(welcomeLabel, switchButton);
+        centerBox.getChildren().addAll(logoView, switchButton);
 
-        setCenter(centerBox);
+        // Background Image
+        ImageView backgroundImage = new ImageView(new Image(new File("assets/Welcome/timmy_detail_ansicht_mund_zu.png").toURI().toString()));
+        backgroundImage.setOpacity(0.4);
+        backgroundImage.setPreserveRatio(true);
+        backgroundImage.setFitWidth(280);
+        backgroundImage.setStyle("-fx-cursor: hand;");
+        StackPane.setAlignment(backgroundImage, Pos.BOTTOM_RIGHT);
+
+        // Main Layout
+        StackPane mainPane = new StackPane(backgroundImage, centerBox);
+        mainPane.getStyleClass().add("view");
+        setCenter(mainPane);
     }
 
     public Button getSwitchButton() {
         return switchButton;
     }
 }
+
